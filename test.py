985d40bebe406b3f557298212fc282eec295e30c
MaheshKO <96043630+MaheshKO@users.noreply.github.com>
2022-02-04 12:32:54 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,13 @@
+def sum(a,b):
+  return a+b
+
+def dif(a,b):
+    return a-b
+  
+n1=input("Enter n1 number")
+n1=int(n1)
+n2=input("enter n2 number")
+n2=int(n2)
+print("sum is:",sum(n1,n2))
+
+print("dif is:",dif(n1,n2))
